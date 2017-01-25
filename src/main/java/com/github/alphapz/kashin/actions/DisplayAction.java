f16f77fb7235d08f22b885c00b2cf58f0b69bcd6
A-pZ <a-pz@h4.dion.ne.jp>
2017-01-25 22:26:20 +0900

    v3.0.0リリース確定版

--- src/main/java/com/github/alphapz/kashin/actions/DisplayAction.java
+++ src/main/java/com/github/alphapz/kashin/actions/DisplayAction.java
@@ -3,11 +3,15 @@
  */
 package com.github.alphapz.kashin.actions;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -21,15 +25,27 @@ import lombok.extern.log4j.Log4j2;
 @ParentPackage("struts-thymeleaf")
 @Results({@Result(name=ActionSupport.SUCCESS,type="thymeleaf",location="index")})
 @Log4j2
-public class DisplayAction extends ActionSupport {
+public class DisplayAction extends ActionSupport implements SessionAware {
 
 	@Action("")
     public String execute() throws Exception {
 		addActionMessage("家臣団能力計算機でっす");
 
-		log.info("アクセスがありました。");
+		Object isAccess = session.get("isAccess");
+		if ( isAccess == null ) {
+			session.put("isAccess", new Date());
+			log.info(" - new session:{}", session);
+		}
 
         return SUCCESS;
     }
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+
+	}
+
+	Map<String, Object> session;
 
 }
